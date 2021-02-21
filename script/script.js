67db82592b493e2b734b4faa2fb15dd77199d4d2
Bobur05 <boxodirovichbb05@gmail.com>
2021-02-21 18:40:21 +0800

    second

---
+++ script/script.js
@@ -0,0 +1,48 @@
+//console.log(1);    
+
+// console.log("Bobur");
+// console.log(5.6);
+// console.log(true);
+// console.log(false);
+
+
+/*var name = "Bobur";
+var age ="16";
+console.log(name);
+console.log(age);
+
+
+var allInfo = ("Я " + name + " мне " + age + " лет !");
+console.log(allInfo);*/
+
+// var str = prompt("Введите свое число");
+// console.log(str);
+
+// alert(str);
+
+
+// console.log("6 + 3 = " + (6 + 3));
+// console.log("6 - 3 = " + (6 - 3));
+// console.log("6 * 3 = " + (6 * 3));
+
+
+var first = +prompt("Решите задачу: 8 + 8 ");
+var second = +prompt("сколько лет Бобуру");
+var third = +prompt(" 9 * 9 ?");
+var four = +prompt("Сколько океанов в Ташкенте");
+
+
+console.log("Правильный ответ 16 , ваш ответ " + first);
+console.log("Правильный ответ 16, ваш ответ " + second);
+console.log(" Правильный ответ 81, ваш ответ " + third);
+console.log(" Правильный ответ 6, ваш ответ " + four);
+
+alert("Зайдите в console");
+
+
+
+
+
+
+
+
